41faa7b2457efa4dd223efd5e575e2344000ecd2
Geraldine Heuse pbf <geraldine.pbf@gmail.com>
2020-07-09 15:04:43 +0200

    init TableView UI

---
+++ BamOnGitHub/Controller/Cells/RepositoryListTableViewCell.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+class RepositoryListTableViewCell : UITableViewCell {
+    
+    @IBOutlet weak var repositoryNameLabel: UILabel!
+    @IBOutlet weak var repositoryLinkButton: UIButton!
+    @IBOutlet weak var repositoryFavButton: UIButton!
+    
+}
