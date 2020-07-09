b3c4ae31daae4cd92355bd5032429d32ed31a967
Geraldine Heuse pbf <geraldine.pbf@gmail.com>
2020-07-09 15:21:30 +0200

    Create TableView Datasource extension with protocol functions Fill Tableview with Data

--- BamOnGitHub/Controller/Cells/RepositoryListTableViewCell.swift
+++ BamOnGitHub/Controller/Cells/RepositoryListTableViewCell.swift
@@ -2,6 +2,7 @@ import UIKit
 
 class RepositoryListTableViewCell : UITableViewCell {
     
+    // MARK: - IBOutlet
     @IBOutlet weak var repositoryNameLabel: UILabel!
     @IBOutlet weak var repositoryLinkButton: UIButton!
     @IBOutlet weak var repositoryFavButton: UIButton!
--- BamOnGitHub/Controller/HomeViewController.swift
+++ BamOnGitHub/Controller/HomeViewController.swift
@@ -10,18 +10,69 @@ import UIKit
 
 class HomeViewController: UIViewController {
     
+    // MARK: - IBOutlet
+    
+    @IBOutlet weak var repositoryListTableView: UITableView!
+    
+    // MARK: - Properties
+    
     /**
      call function from the API
      */
     let api = APIClient()
     
+    /**
+     Store GitHub repositories data
+     */
+    var repositories: [GitHubRepository] = []
+    
+    // MARK: - overrided functions
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.repositoryListTableView.dataSource = self
         api.getRepositories() { (data: [GitHubRepository]) in
-            print(data)
+            self.repositories = data
+            DispatchQueue.main.async {
+                self.repositoryListTableView.reloadData()
+            }
         }
     }
     
+    // MARK: - HomeViewController functions
     
+    /**
+     * Open selected repository
+     - Parameter sender: Link Button
+     */
+    @objc func buttonLinkClicked(sender: UIButton) {
+        print("Button Link clicked")
+    }
+    
+    /**
+     * Add selected repository to favorites
+     - Parameter sender: Fav Button
+     */
+    @objc func buttonFavClicked(sender: UIButton) {
+        print("Button Fav clicked")
+    }
+}
+
+// MARK: - Extension : TableView DataSource
+
+extension HomeViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        self.repositories.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "repositoryListCell") as! RepositoryListTableViewCell
+        
+        let repository = self.repositories[indexPath.row]
+        cell.repositoryNameLabel.text = repository.name
+        cell.repositoryLinkButton.addTarget(self, action: #selector(buttonLinkClicked(sender:)), for: .touchUpInside)
+        cell.repositoryFavButton.addTarget(self, action: #selector(buttonFavClicked(sender:)), for: .touchUpInside)
+        return cell
+        
+    }
 }
 
