2cadf53fbb15b73c38f02b85035d5f2ef8faf58d
Geraldine Heuse pbf <geraldine.pbf@gmail.com>
2020-07-09 16:41:05 +0200

    Start of unit tests

--- BamOnGitHubTests/BamOnGitHubTests.swift
+++ BamOnGitHubTests/BamOnGitHubTests.swift
@@ -10,6 +10,8 @@ import XCTest
 @testable import BamOnGitHub
 
 class BamOnGitHubTests: XCTestCase {
+    
+    let config = AppConfig()
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -22,6 +24,12 @@ class BamOnGitHubTests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func test_API_BaseURLString_IsCorrect() {
+        let URLString = config.APIURL
+        let expectedURLString = "https://api.github.com/orgs/bamlab/repos"
+        XCTAssertEqual(URLString, expectedURLString, "URL does not match expected URL. Expected URLs to match.")
     }
 
     func testPerformanceExample() {
